0995c3a817bb5cdc13365c9ec60d0598ac451a55
AnuugacchatiPravaha95 <87059251+AnuugacchatiPravaha95@users.noreply.github.com>
2022-02-19 07:39:01 -0500

    Add files via upload

---
+++ ProductMVC/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductMVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { set; get; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="Enter Product name")]
+        public string ProductName { set; get; }
+        [Range(8,50,ErrorMessage ="Rate Should be between 8 to 50")]
+        public decimal Rate { set; get; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Enter Product Description")]
+        public string Description { set; get; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Enter Product  CategoryName")]
+        public string CategoryName{set;get;}
+    }
+}
